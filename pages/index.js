d38c190c40a3e8af83844d365379e95eef420740
Wendy Zhang <wendyzhang250@gmail.com>
2022-11-24 12:29:44 -0800

    updated index.js

--- pages/index.js
+++ pages/index.js
@@ -12,24 +12,15 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
         <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
+          <a href="/task-1.js" className={styles.card}>
+            <h2> Task 1 </h2>
+            <p>Front End </p>
           </a>
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
+          <a href="/task-2.js" className={styles.card}>
+            <h2> Task 2 </h2>
+            <p>Backend</p>
           </a>
 
           <a
